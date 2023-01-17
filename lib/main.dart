8dd67cade93938d7c9471342c9cac4434c380995
Nafie <mostafa.nafie@yassir.com>
2023-01-17 09:26:35 +0200

    ♻️ Separate the dialog

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:batata_hackathon/widgets/app_buttons.dart';
+import 'package:batata_hackathon/widgets/dialog.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
@@ -92,31 +93,4 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
     );
   }
-}
-
-showAlertDialog(BuildContext context, String title, String content) {
-  // set up the button
-  Widget okButton = TextButton(
-    child: Text("OK"),
-    onPressed: () {
-      Navigator.of(context).pop();
-    },
-  );
-
-  // set up the AlertDialog
-  AlertDialog alert = AlertDialog(
-    title: Text(title),
-    content: Text(content),
-    actions: [
-      okButton,
-    ],
-  );
-
-  // show the dialog
-  showDialog(
-    context: context,
-    builder: (BuildContext context) {
-      return alert;
-    },
-  );
 }
