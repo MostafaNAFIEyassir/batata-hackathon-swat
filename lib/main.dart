e2d5da8264f59d7de3c03ab9f2003019feaf5104
Nafie <mostafa.nafie@yassir.com>
2023-01-17 09:08:17 +0200

    🧹 Remove comments.

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,6 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return ChangeNotifierProvider(
@@ -18,15 +17,6 @@ class MyApp extends StatelessWidget {
       child: MaterialApp(
         title: 'Flutter Demo',
         theme: ThemeData(
-          // This is the theme of your application.
-          //
-          // Try running your application with "flutter run". You'll see the
-          // application has a blue toolbar. Then, without quitting the app, try
-          // changing the primarySwatch below to Colors.green and then invoke
-          // "hot reload" (press "r" in the console where you ran "flutter run",
-          // or simply save your changes to "hot reload" in a Flutter IDE).
-          // Notice that the counter didn't reset back to zero; the application
-          // is not restarted.
           primarySwatch: Colors.blue,
         ),
         home: const MyHomePage(title: 'Batata Hackathon 🥔'),
@@ -41,18 +31,7 @@ class MyAppState extends ChangeNotifier {
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
-
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
@@ -70,51 +49,20 @@ class _MyHomePageState extends State<MyHomePage> {
       });
     });
 
-    @override
-    void dispose() {
-      // Clean up the controller when the widget is removed from the
-      // widget tree.
-      controller.dispose();
-      super.dispose();
-    }
-
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    var inputDecoration = const InputDecoration(
+    const textFieldInputDecoration = InputDecoration(
         border: OutlineInputBorder(),
         labelText: 'How much kebda do you need?',
         hintText: 'Amount of kebda');
+
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.start,
           children: <Widget>[
-            SizedBox(height: 50),
+            const SizedBox(height: 50),
             Text("SWAT 😈",
                 style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold)),
             SizedBox(height: 150),
@@ -123,12 +71,9 @@ class _MyHomePageState extends State<MyHomePage> {
               child: TextFormField(
                 keyboardType: TextInputType.number,
                 inputFormatters: <TextInputFormatter>[
-                  // for below version 2 use this
-                  // FilteringTextInputFormatter.allow(RegExp(r'[0-9]')),
-// for version 2 and greater youcan also use this
                   FilteringTextInputFormatter.digitsOnly
                 ],
-                decoration: inputDecoration,
+                decoration: textFieldInputDecoration,
                 controller: controller,
               ),
             ),
@@ -138,14 +83,13 @@ class _MyHomePageState extends State<MyHomePage> {
               press: appState.isEnabled
                   ? () {
                       showAlertDialog(
-                          context, 'Batata Hackathon 🥔', "Enjoy your kebda!");
+                          context, widget.title, "Enjoy your kebda!");
                     }
                   : null,
-              isEnabled: appState.isEnabled,
             )
           ],
         ),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
     );
   }
 }
@@ -155,7 +99,7 @@ showAlertDialog(BuildContext context, String title, String content) {
   Widget okButton = TextButton(
     child: Text("OK"),
     onPressed: () {
-      Navigator.of(context).pop(); // dismiss dialog
+      Navigator.of(context).pop();
     },
   );
 
